10e737d2cba8f715929b183c50072578e5d87161
BahaaDagher <bahaa.dagher99@gmail.com>
2023-04-21 02:28:22 +0200

    the App becomes interactive

---
+++ src/components/Questions.jsx
@@ -0,0 +1,31 @@
+const Questions = [
+    {
+        questionText: 'What is the capital of France?',
+        answerOptions: [ 'New York' , 'London' ,  'Paris',  'Dublin' ] , 
+        correctAnswer:   2 , 
+    },
+    {
+        questionText: 'Who is CEO of Tesla?',
+        answerOptions: [ 'Jeff Bezos' , 'Elon Musk' ,  'Bill Gates' ,  'Tony Stark' ] ,
+        correctAnswer:   1 ,
+    } , 
+    {
+        questionText: 'The iPhone was created by which company?',
+        answerOptions: [ 'Apple' , 'Intel' ,  'Amazon' ,  'Microsoft' ] ,
+        correctAnswer:   0 ,
+    } ,
+    {
+        questionText: 'How many Harry Potter books are there?',
+        answerOptions: [ '1' , '4' ,  '6' ,  '7' ] ,
+        correctAnswer:   3 ,
+    } ,
+    {
+        questionText: 'What is the most used programming language in 2020?',
+        answerOptions: [ 'Java' , 'C' ,  'Python' ,  'JavaScript' ] ,
+        correctAnswer:   3 ,
+    } ,
+
+
+] ;
+
+export default Questions;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,111 +1,158 @@
 import styled from "@emotion/styled";
+import Questions from "./Questions";
+import { useState } from "react";
 
-const Container = styled("div")(({theme}) =>( {
-    fontFamily:"cursive",
-    // background: "linear-gradient(190deg, #7cc6fe 0%, rgba(0,26,63,1) 100%)" , 
-    backgroundColor : "#7cc6fe" , 
-    width: "90%" ,
-    height : "500px" ,
-    margin  : "auto" ,
-    marginTop : "70px" ,
-    display : "flex" ,
-    justifyContent : "center" ,
-    alignItems : "center" , 
-    justifyContent: "space-around" , 
-    borderRadius : "20px" ,
-}))
-const AppName = styled("h1")(({theme}) =>( {
-    width : "30%" ,
-    color : "#252d4a" ,
-    display : "flex" ,
-    justifyContent : "center" ,
-    alignItems : "center" ,
-    fontSize : "70px" ,
-    fontWeight : "bold" ,
+const Container = styled("div")(({ theme }) => ({
+  fontFamily: "cursive",
+  background:
+    "linear-gradient(190deg, rgba(124,198,254,1) 0%, rgba(255,255,255,1) 100%)",
+  padding: "40px",
+  backgroundColor: "#FFF",
+  width: "90%",
+  margin: "auto",
+  marginTop: "70px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  justifyContent: "space-around",
+  borderRadius: "20px",
+  boxShadow: "0px 3px 20px #252d4a",
+}));
+const AppName = styled("h1")(({ theme }) => ({
+  width: "30%",
+  color: "#252d4a",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "70px",
+  fontWeight: "bold",
+}));
+const QuizContainer = styled("div")(({ theme }) => ({
+  width: "60%",
+  backgroundColor: "#252d4a",
+  color: "#ebecee",
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "70px",
+  alignItems: "center",
+  borderRadius: "20px",
+  boxShadow: "0px 10px 30px #252d4a",
+}));
+const QuestionContainer = styled("h2")(({ theme }) => ({
+  width: "45%",
+  color: "#ebecee",
+  height: "200px",
+  marginRight: "20px",
+}));
+const Question = styled("h3")(({ theme }) => ({
+  marginTop: "30px",
+  fontWeight: "bold",
+}));
+const NumberOfQuestion = styled("span")(({ theme }) => ({
+  fontSize: "20px",
+  fontWeight: "bold",
+  color: "#7cc6fe",
+}));
+const AnswersContainer = styled("div")(({ theme }) => ({
+  width: "55%",
+  color: "#ebecee",
+  marginLeft: "20px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+}));
 
-}))
-const QuizContainer  = styled("div")(({theme}) =>( {
-    width : "60%" ,
-    backgroundColor : "#252d4a" ,
-    color : "#ebecee" ,
-    display : "flex" ,
-    justifyContent : "space-between" ,
-    padding : "70px" ,
-    alignItems : "center" ,
-    borderRadius : "20px" ,
-    boxShadow : "0px 10px 30px #252d4a"   , 
-    
-}))
-const QuestionContainer  = styled("h2")(({theme}) =>( {
-    width : "45%" ,
-    color : "#ebecee" ,
-    height : "200px" , 
-    marginRight : "20px" ,
-    
-}))
-const Question  = styled("h2")(({theme}) =>( {
-    marginTop : "20px" ,
-    fontWeight : "bold" ,
-}))
-const NumberOfQuestion  = styled("span")(({theme}) =>( {
-    fontSize : "20px" ,
-    fontWeight : "bold" ,
-    color: "yellow" ,
-}))
-const AnswersContainer  = styled("div")(({theme}) =>( {
-    width : "55%" ,
-    color : "#ebecee" ,
-    marginLeft: "20px" ,
-    display : "flex" ,
-    flexDirection : "column" ,
-    justifyContent : "center" ,
+const Answer = styled("div")(({ theme }) => ({
+  border: "4px solid #234668",
+  borderRadius: "20px",
+  margin: "10px",
+  padding: "10px",
+  fontWeight: "bold",
+  fontSize: "20px",
+  transition:
+    "background-color 0.2s ease-in-out, color 0.2s ease-in-out , border 0.2s ease-in-out",
+  "&:hover": {
+    backgroundColor: "#234668",
+    cursor: "pointer",
+    borderColor: "#7cc6fe",
+  },
+}));
 
-    
-}))
+const ResultContainer = styled(QuizContainer)(({ theme }) => ({
+    display : "flex",
+    flexDirection : "column",
+    justifyContent : "center",
+    alignItems : "center",
+}));
+const ResultScore = styled(AppName)(({ theme }) => ({
+    width: "100%",
+    color: "#FFF",
+}));
 
-const Answer  = styled("div")(({theme}) =>( {
-    border: "4px solid #234668 " ,
-    borderRadius : "20px" ,
-    margin : "10px" ,
-    padding : "10px" ,
-    fontWeight : "bold" ,
-    fontSize : "20px" ,
-    transition : "background-color 0.2s ease-in-out, color 0.2s ease-in-out , border 0.2s ease-in-out" , 
-    '&:hover' :{
-        backgroundColor : "#234668" ,
-        color : "#7cc6fe" ,
-        cursor : "pointer" ,
-        borderColor : "#7cc6fe" ,
-    }
+const AgainButton = styled("siv")(({ theme }) => ({
+    fontWeight: "bold",
+    fontSize: "20px",
+    width: "20%",
+    height: "50px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: "0px 4px 10px 1px rgba(0, 0, 0, 0.3)",
+    backgroundColor: "#f24572 ",
+    borderRadius: "10px",
+    transition: "background-color 0.2s ease",
+    marginTop: "20px",
+    color: "#fff",
+    "&:hover": {
+        cursor: "pointer",
+        backgroundColor: "#f24572d9",
+    },
+
+}));
+
+const Quiz = () => {
+  const [index, setIndex] = useState(0);
+    const [result, setResult] = useState(0);
+    const [finish , setFinish] = useState(false);
 
 
-}))
-
-
-
-
-const Quiz = ()=>{
-return (
+  const next = (i) => {
+    if (Questions[index]["correctAnswer"]==i) setResult(result+1);
+    if (index === Questions.length-1) setFinish(true);
+    else setIndex(index + 1);
+  };
+  const Agin = () =>{
+    setFinish(false);
+    setIndex(0);
+    setResult(0);
+  }
+  return (
     <>
-        <Container>
-            <AppName>
-                Quiz App
-            </AppName>
-            <QuizContainer>
-                <QuestionContainer>
-                    Question  1 /<NumberOfQuestion> 4 </NumberOfQuestion>
-                    <Question>what is my name ?</Question>
-                </QuestionContainer>
-                <AnswersContainer>
-                    <Answer>bahaa mohamed dagher</Answer>
-                    <Answer>mohamed mohsen sayed ibrahim   </Answer>
-                    <Answer>ahmed alaa ElDin mohamed </Answer>
-                    <Answer>Esma3el mahmoud sha3ban </Answer> 
-                </AnswersContainer>
-            </QuizContainer>
-
-        </Container>
+      <Container>
+        <AppName>Quiz App</AppName>
+        { finish ? ( 
+            <ResultContainer>
+                <ResultScore> your result is ( {result} ) </ResultScore>
+                <AgainButton onClick = { ()=> Agin()}>Agin</AgainButton>
+            </ResultContainer>
+        ) : (
+        <QuizContainer>
+          <QuestionContainer>
+            Question {index + 1} /
+            <NumberOfQuestion> {Questions.length} </NumberOfQuestion>
+            <Question> {Questions[index]["questionText"]}</Question>
+          </QuestionContainer>
+          <AnswersContainer>
+            {Questions[index]["answerOptions"].map((answer , i ) => {
+              return (
+                <Answer  key = {i} onClick={() => { next(i); }} > {answer} </Answer>
+              );
+            })}
+          </AnswersContainer>
+        </QuizContainer>
+        )}
+      </Container>
     </>
-)
-}
+  );
+};
 export default Quiz;
